4893b0b9deab384db466b9015760b27a0ebfbfb4
guilhermerodrigues680 <guilhermerodrigues680@gmail.com>
2020-04-11 15:11:04 -0300

    v0.2

--- pywav2c.py
+++ pywav2c.py
@@ -1,61 +1,66 @@
 import wave
-from time import sleep
+import sys
 
 
-audiowav = wave.open('dm3000.wav', mode='rb')
-
-
-sample_rate = audiowav.getframerate()
-aux1 = audiowav.getnframes()
-aux = audiowav.getparams()
-
-print(aux)
-
-amostras = []
-while True:
-    sample = audiowav.readframes(1)
+def main(argv):
     
-    if sample == b'':
-        break
-    else:
-        inteiro = int.from_bytes(sample, byteorder='little', signed=False)
-        # print(sample.hex())
-        # print(inteiro)
-        amostras.append(inteiro)
+    if len(argv) < 3:
+        print("Uso 1: python3 pywav2c.py <file.wav> <output.c> <soundname>")
+        # print("Uso 2: python3 pywav2c.py <file.wav> <output.c> <soundname> <amount of samples>")
+        sys.exit(0)
 
-f = open("sounddata.h", "w")
+    FILE_IN = argv[0]
+    FILE_OUT = argv[1]
+    SOUND_NAME = argv[2]
+    AUDIO_PARAMS = None
+    amostras = []
 
-cabecalho = '''// Wav2c recriado em Python3 por Guilherme Rodrigues: https://github.com/guilhermerodrigues680/py-wav2c
-// Baseado no wav2c em C de Olle Jonsson: https://github.com/olleolleolle/wav2c\n
-'''
+    with wave.open(FILE_IN, mode='rb') as audiowav:
+        AUDIO_PARAMS = audiowav.getparams()
+        print(AUDIO_PARAMS)
 
-# f.write("// Wav2c recriado em Python3 por Guilherme Rodrigues: https://github.com/guilhermerodrigues680/py-wav2c \n\n")
-# f.write("// Baseado no wav2c em C de Olle Jonsson: https://github.com/olleolleolle/wav2c \n")
-f.write(cabecalho)
-f.write("const int sounddata_sampleRate = " + str(sample_rate) + "; \n")
-f.write("const int sounddata_length = " + str(len(amostras)) + "; \n\n")
-f.write("const unsigned char sounddata_data[] PROGMEM ={")
+        while True:
+            sample = audiowav.readframes(1)
+            
+            if sample == b'':
+                break
+            
+            inteiro = int.from_bytes(sample, byteorder='little', signed=False)
+            amostras.append(inteiro)
 
-for idx, amostra in enumerate(amostras):
-    
-    if len(str(amostra)) == 1:
-        f.write('  ')
-    elif len(str(amostra)) == 2:
-        f.write(' ')
-    
-    f.write(str(amostra))
-
-    if (idx + 1) != len(amostras):
+    with open(FILE_OUT, "w") as f:
         
-        if idx % 20 == 0:
-            f.write(',\n')
-        else:
-            f.write(', ')
+        cabecalho = (
+            "// Wav2c recriado em Python3 por Guilherme Rodrigues: https://github.com/guilhermerodrigues680/py-wav2c\n"
+            "// Baseado no wav2c em C de Olle Jonsson: https://github.com/olleolleolle/wav2c\n\n"
+            "// const int " + str(SOUND_NAME) + "_sampleRate = " + str(AUDIO_PARAMS.framerate) + "; \n"
+            "const int " + str(SOUND_NAME) + "_length = " + str(len(amostras)) + "; \n\n"
+            "const unsigned char " + str(SOUND_NAME) + "_data[] PROGMEM ={"
+        )
 
-    else:
-        f.write(" };\n")
+        f.write(cabecalho)
+
+        for idx, amostra in enumerate(amostras):
+            
+            if len(str(amostra)) == 1:
+                f.write('  ')
+            elif len(str(amostra)) == 2:
+                f.write(' ')
+            
+            f.write(str(amostra))
+
+            if (idx + 1) != len(amostras):
+                
+                if idx % 20 == 0:
+                    f.write(',\n')
+                else:
+                    f.write(', ')
+
+            else:
+                f.write(" };\n")
+  
+    print('-> Conversao finalizada')
 
 
-
-f.close()  
-print('Conversao finalizada')
+if __name__ == "__main__":
+    main(sys.argv[1:])
